cb52bce4fff247326a9f6b39f2b4796526283765
AndresG0C <andresgonzalez22004@gmail.com>
2025-03-11 15:58:54 -0500

    App funcionando bien con navbar

--- econoengine/lib/Views/navbar_view.dart
+++ econoengine/lib/Views/navbar_view.dart
@@ -1,17 +1,17 @@
 import 'package:econoengine/Views/home_view.dart';
 import 'package:econoengine/Views/Profile_view.dart';
-import 'package:econoengine/Views/TIR_view.dart';
-import 'package:econoengine/Views/UVR_view.dart';
-import 'package:econoengine/Views/alt_inv_view.dart';
-import 'package:econoengine/Views/amortizacion_view.dart';
-import 'package:econoengine/Views/bonos_view.dart';
-import 'package:econoengine/Views/gradientes_view.dart';
-import 'package:econoengine/Views/inflacion_view.dart';
+// import 'package:econoengine/Views/TIR_view.dart';
+// import 'package:econoengine/Views/UVR_view.dart';
+// import 'package:econoengine/Views/alt_inv_view.dart';
+// import 'package:econoengine/Views/amortizacion_view.dart';
+// import 'package:econoengine/Views/bonos_view.dart';
+// import 'package:econoengine/Views/gradientes_view.dart';
+// import 'package:econoengine/Views/inflacion_view.dart';
 import 'package:econoengine/Views/settings_view.dart';
 import 'package:econoengine/Views/transactions_view.dart';
 import 'package:flutter/material.dart';
-import 'package:econoengine/Views/interesSimple_view.dart';
-import 'package:econoengine/Views/interesCompuesto_view.dart';
+// import 'package:econoengine/Views/interesSimple_view.dart';
+// import 'package:econoengine/Views/interesCompuesto_view.dart';
 
 class NavbarView extends StatefulWidget {
   const NavbarView({super.key});
@@ -21,63 +21,57 @@ class NavbarView extends StatefulWidget {
 }
 
 class _NavbarViewState extends State<NavbarView> {
-  int _selectedIndex = 0; // Índice para controlar la opción seleccionada
-  final List<GlobalKey<NavigatorState>> _navigatorKeys = [
-    GlobalKey<NavigatorState>(),
-    GlobalKey<NavigatorState>(),
-    GlobalKey<NavigatorState>(),
-    GlobalKey<NavigatorState>(),
-  ];
+  int _selectedIndex = 0; // Índice de la pestaña seleccionada
+  final List<GlobalKey<NavigatorState>> _navigatorKeys = List.generate(
+    4,
+    (index) => GlobalKey<NavigatorState>(),
+  );
 
-  // Método para manejar el cambio de índice desde el BottomNavigationBar
+  // Maneja el cambio de pestañas
   void _onItemTapped(int index) {
-  // Restablece la navegación dentro de la pestaña seleccionada
-  _navigatorKeys[index].currentState?.popUntil((route) => route.isFirst);
+    if (_selectedIndex == index) {
+      // Si el usuario toca la pestaña actual, vuelve al inicio de la pila de navegación
+      _navigatorKeys[index].currentState?.popUntil((route) => route.isFirst);
+    } else {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
 
-  setState(() {
-    _selectedIndex = index;
-  });
-}
-
-  // Método para construir las rutas de cada pestaña
-  Map<String, WidgetBuilder> _routeBuilders(BuildContext context, int index) {
-    return {
-      '/': (context) {
-        return [
-          const HomeView(), // Vista principal
-          const SettingsView(), // Vista de ajustes
-          const ProfileView(), // Vista de perfil
-          const TransactionsView(), // Vista de movimientos
-        ][index];
-      },
-      '/interes-simple': (context) => const InteresSimpleView(),
-      '/interes-compuesto': (context) => const InteresCompuestoView(),
-      '/gradientes': (context) => const GradientesView(),
-      '/amortizacion': (context) => const AmortizacionView(),
-      '/TIR': (context) => const TIRView(),
-      '/UVR': (context) => const UVRView(),
-      '/alternativasInversion': (context) => const AlternativasInversionView(),
-      '/bonos': (context) => const BonosView(),
-      '/inflacion': (context) => const InflacionView(),
-    };
+  // Construcción de rutas dentro de cada pestaña
+  Widget _buildScreen(int index) {
+    return [
+      const HomeView(),
+      const SettingsView(),
+      const ProfileView(),
+      const TransactionsView(),
+    ][index];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [
-          _buildOffstageNavigator(0),
-          _buildOffstageNavigator(1),
-          _buildOffstageNavigator(2),
-          _buildOffstageNavigator(3),
-        ],
+        children: List.generate(4, (index) {
+          return Offstage(
+            offstage: _selectedIndex != index,
+            child: Navigator(
+              key: _navigatorKeys[index],
+              onGenerateRoute: (settings) {
+                return MaterialPageRoute(
+                  builder: (context) => _buildScreen(index),
+                );
+              },
+            ),
+          );
+        }),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex, // Índice seleccionado
-        onTap: _onItemTapped, // Método para manejar el cambio de índice
-        selectedItemColor: Colors.blue[800], // Color del ícono seleccionado
-        unselectedItemColor: Colors.grey, // Color del ícono no seleccionado
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue[800],
+        unselectedItemColor: Colors.grey,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -96,23 +90,6 @@ class _NavbarViewState extends State<NavbarView> {
             label: 'Movimientos',
           ),
         ],
-      ),
-    );
-  }
-
-  // Método para construir un Navigator offstage para cada pestaña
-  Widget _buildOffstageNavigator(int index) {
-    return Offstage(
-      offstage: _selectedIndex != index,
-      child: Navigator(
-        key: _navigatorKeys[index],
-        onGenerateRoute: (settings) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return _routeBuilders(context, index)[settings.name]!(context);
-            },
-          );
-        },
       ),
     );
   }
--- econoengine/lib/main.dart
+++ econoengine/lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:econoengine/Views/Auth/forgot_password_view.dart';
 // import 'package:econoengine/Views/Auth/home_view.dart';
 import 'package:econoengine/Views/Auth/register_view.dart';
-import 'package:econoengine/Views/TIR_view.dart';
-import 'package:econoengine/Views/UVR_view.dart';
-import 'package:econoengine/Views/alt_inv_view.dart';
-import 'package:econoengine/Views/amortizacion_view.dart';
-import 'package:econoengine/Views/bonos_view.dart';
-import 'package:econoengine/Views/gradientes_view.dart';
-import 'package:econoengine/Views/inflacion_view.dart';
-import 'package:econoengine/Views/interesCompuesto_view.dart';
-import 'package:econoengine/Views/interesSimple_view.dart';
 import 'package:econoengine/Views/navbar_view.dart';
 import 'package:flutter/material.dart';
 import 'views/auth/login_view.dart';
@@ -43,15 +34,6 @@ class MyApp extends StatelessWidget {
         '/register': (context) => const RegisterView(),
         '/home': (context) => const NavbarView(),
         '/recoverPassword': (context) => const ForgotPasswordView(),
-        '/interesSimple': (context) => const InteresSimpleView(),
-        '/interesCompuesto': (context) => const InteresCompuestoView(),
-        '/gradientes': (context) => const GradientesView(),
-        '/amortizacion': (context) => const AmortizacionView(),
-        '/TIR': (context) => const TIRView(),
-        '/UVR': (context) => const UVRView(),
-        '/alternativasInversion': (context) => const AlternativasInversionView(),
-        '/bonos': (context) => const BonosView(),
-        '/inflacion': (context) => const InflacionView(),
       },
     );
   }
